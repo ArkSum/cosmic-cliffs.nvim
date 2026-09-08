fca95d612bcfe4fbe51b2935e303fb66f45770fc
Markus Klawun <mklawun@outlook.com>
2026-09-07 22:54:45 -0500

    undo reordering

--- lua/cosmic-cliffs/init.lua
+++ lua/cosmic-cliffs/init.lua
@@ -153,6 +153,16 @@ function M.setup()
 	set(0, "Pmenu", { bg = colors.gray_3, fg = colors.ansiWhite })
 	-- PMENUSEL_BG = STATUSLINE_NORMAL_BG
 	set(0, "PmenuSel", { bg = colors.gray_4, fg = colors.black })
+	-- STATUSLINE = Line of text at the bottom of the interface
+  -- Uses the lualine plugin, so those are the highlight groups used
+	set(0, "lualine_a_normal", { bg = "#ff0000", fg = "#ffffff" })
+  set(0, "lualine_transitional_lualine_a_normal_to_lualine_b_normal", { bg = "#ffffff", fg = "#ff0000" } )
+	set(0, "lualine_b_normal", { bg = "#ffffff", fg = "#ff0000" })
+  set(0, "lualine_transitional_lualine_b_normal_to_lualine_c_normal", { bg = "#ff0000", fg = "#ffffff" } )
+	set(0, "lualine_c_normal", { bg = "#ff0000", fg = "#ffffff" })
+	set(0, "lualine_x_normal", { bg = "#ff0000", fg = "#ffffff" })
+	set(0, "lualine_y_normal", { bg = "#ff0000", fg = "#ffffff" })
+	set(0, "lualine_z_normal", { bg = "#ff0000", fg = "#ffffff" })
 
 	set(0, "CursorLine", { bg = colors.gray_1 })
 	set(0, "Visual", { bg = colors.gray_2 })
@@ -195,16 +205,6 @@ function M.setup()
 	set(0, "NeoTreeNormal", { fg = colors.normal })
 	set(0, "Directory", { fg = colors.gray_6 })
 
-	-- STATUSLINE = Line of text at the bottom of the interface
-  -- Uses the lualine plugin, so those are the highlight groups used
-	set(0, "lualine_a_normal", { bg = "#ff0000", fg = "#ffffff" })
-  set(0, "lualine_transitional_lualine_a_normal_to_lualine_b_normal", { bg = "#ffffff", fg = "#ff0000" } )
-	set(0, "lualine_b_normal", { bg = "#ffffff", fg = "#ff0000" })
-  set(0, "lualine_transitional_lualine_b_normal_to_lualine_c_normal", { bg = "#ff0000", fg = "#ffffff" } )
-	set(0, "lualine_c_normal", { bg = "#ff0000", fg = "#ffffff" })
-	set(0, "lualine_x_normal", { bg = "#ff0000", fg = "#ffffff" })
-	set(0, "lualine_y_normal", { bg = "#ff0000", fg = "#ffffff" })
-	set(0, "lualine_z_normal", { bg = "#ff0000", fg = "#ffffff" })
 
 end
 
